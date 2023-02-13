ca21b5ff410747637d834aef10d9f3c8555c81a3
hayaalsafady <haiasafadi094@gmail.com>
2023-02-13 14:54:12 +0200

    update

--- human_resource/human/doctype/attendance/attendance.py
+++ human_resource/human/doctype/attendance/attendance.py
@@ -1,8 +1,7 @@
 # Copyright (c) 2023, GSG and contributors
 # For license information, please see license.txt
-import self as self
+from datetime import timedelta, datetime
 
-import frappe
 import frappe
 from frappe.model.document import Document
 from frappe.utils.data import time_diff_in_hours
@@ -11,24 +10,73 @@ from frappe.utils.data import time_diff_in_hours
 class Attendance(Document):
     def validate(self):
         self.get_work_hour()
+        self.get_late_hours()
+        self.update_status()
 
-    def get_work_hour(self):
-        work_hours = time_diff_in_hours(self.check_out ,self. check_in )
+    def get_work_hour(self, check_in=None, check_out=None):
+        work_hours = time_diff_in_hours(self.check_out, self. check_in)
         frappe.msgprint(str(work_hours))
         self.work_hours = work_hours
 
+        late_entry = frappe.db.get_single_value('Attendance Settings', 'late_entry_grace_period')
+        early_exit = frappe.db.get_single_value('Attendance Settings', 'early_exit_grace_period')
+        end_time = frappe.db.get_single_value('Attendance Settings', 'end_time')
+        start_time = frappe.db.get_single_value('Attendance Settings', 'start_time')
 
-        #late_entry = frappe.db.get_single_value('Attendance Settings', 'late_entry_grace_period')
-        #early_exit= frappe.db.get_single_value('Attendance Settings', 'early_exit_grace_period')
-        #end_time= frappe.db.get_single_value('Attendance Settings', 'end_time')
-        #start_time = frappe.db.get_single_value('Attendance Settings', 'start_time')
+        start_time_with_late_entry = start_time + timedelta(minutes=late_entry)
+        end_time_with_early_exit= end_time - timedelta(minutes=early_exit)
+
+        total_start_time = datetime.strptime(str(start_time_with_late_entry), "%H:%M:%S")
+
+        total_end_time = datetime.strptime(str(end_time_with_early_exit), "%H:%M:%S")
+
+        if check_in <= total_start_time:
+            check_in = datetime.strptime(str(start_time), "%H:%M:%S")
+
+        else:
+            check_in = check_in - timedelta(minutes=late_entry)
 
 
-        #self.check_in
-        #self.check_out
+        if check_out >= total_end_time:
+            check_out = datetime.strptime(str(end_time), "%H:%M:%S")
+        else:
+
+            check_out = check_out - timedelta(minutes=early_exit)
 
 
-#         start_time + late_entry
-#          end_time - early_exit
+        check_sum = check_out - check_in
+        second = check_sum.total_seconds()
+        work_hours = second / (60*60)
+        self.work_hours = work_hours
+
+
+    def get_late_hours(self):
+        end_time = frappe.db.get_single_value('Attendance Settings', 'end_time')
+        start_time = frappe.db.get_single_value('Attendance Settings', 'start_time')
+
+        start_time = datetime.strptime(start_time, "%H:%M:%S")
+        end_time = datetime.strptime(end_time, "%H:%M:%S")
+
+        sum_working_hours_threshold = end_time - start_time
+        second = sum_working_hours_threshold.total_seconds()
+        working_hours_threshold = second / (60*60)
+        self.late_hours = working_hours_threshold - self.work_hours
+
+
+    def update_status(self):
+        working_hours_threshold_for_absent = frappe.db.get_single_value('Attendance Settings', 'working_hours_threshold_for_absent')
+
+        if self.late_hours >= working_hours_threshold_for_absent:
+            self.status= 'Absent'
+
+
+
+
+
+
+
+
+
+
 
 
--- human_resource/human/doctype/employee_education/employee_education.py
+++ human_resource/human/doctype/employee_education/employee_education.py
@@ -3,10 +3,8 @@
 import datetime
 
 import frappe
-import frappe
 from frappe.model.document import Document
 from frappe.utils import getdate
-
 
 
 def get_today():
@@ -15,23 +13,23 @@ def get_today():
 
 class EmployeeEducation(Document):
     def get_age(self):
-           if self.date_of_birth < get_today():
-              self.age = int((getdate(get_today())-getdate(self.date_of_birth)).day/366)
-           else:
-               frappe.throw("the dob can not be set as today date")
+     if self.date_of_birth < get_today():
+        self.age = int((getdate(get_today()) - getdate(self.date_of_birth)).day / 356)
+     else:
+        frappe.throw("the dob can not be set as today date")
 
     def validate_age(self):
-          if self.age >= 60 and self.status =="Active":
-              frappe.throw("Age should be less than 60")
+        if self.age >= 60 and self.status == "Active":
+            frappe.throw("Age should be less than 60")
 
     def validate_mobile(self):
-         if len(self.mobile)==10 :
-             if self.mobile.startwith('05') :
+        if len(self.mobile) == 10:
+             if self.mobile.startwith('05'):
                  pass
              else:
                  frappe.throw("mobile num should start with 05")
-         else:
-             frappe.throw("mobile number should be 10 digital num")
+        else:
+            frappe.throw("mobile number should be 10 digital num")
 
 
 
