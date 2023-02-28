0bcf0b0fc3b5656adb1cba9f6bcac006517ff2fd
hayaalsafady <haiasafadi094@gmail.com>
2023-02-28 19:33:37 +0200

    update

--- human_resource/human/doctype/employee_education/employee_education.py
+++ human_resource/human/doctype/employee_education/employee_education.py
@@ -6,17 +6,18 @@ import frappe
 from frappe.model.document import Document
 from frappe.utils import getdate
 
-
+# test
 def get_today():
     today = datetime.datetime.now().strftime("%Y-%m-%d")
     return today
 
+
 class EmployeeEducation(Document):
     def get_age(self):
-     if self.date_of_birth < get_today():
-        self.age = int((getdate(get_today()) - getdate(self.date_of_birth)).day / 356)
-     else:
-        frappe.throw("the dob can not be set as today date")
+        if self.date_of_birth < get_today():
+            self.age = int((getdate(get_today()) - getdate(self.date_of_birth)).day / 356)
+        else:
+            frappe.throw("the dob can not be set as today date")
 
     def validate_age(self):
         if self.age >= 60 and self.status == "Active":
@@ -24,12 +25,9 @@ class EmployeeEducation(Document):
 
     def validate_mobile(self):
         if len(self.mobile) == 10:
-             if self.mobile.startwith('05'):
-                 pass
-             else:
-                 frappe.throw("mobile num should start with 05")
+            if self.mobile.startwith('05'):
+                pass
+            else:
+                frappe.throw("mobile num should start with 05")
         else:
             frappe.throw("mobile number should be 10 digital num")
-
-
-
